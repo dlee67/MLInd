dfbfe5c8e19c21da88c2a1994596a8d47ae74e33
bob <dlee67@msudenver.edu>
2018-01-24 17:48:09 -0700

    01/24/18

--- FirstAssig/AssigOne.py
+++ FirstAssig/AssigOne.py
@@ -11,17 +11,15 @@ targetValues = [[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0],
 			  [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1]]
 
 inputValues = inputGenerator.generateInput(len(targetValues))
-
-print(inputValues)
 			  
-#def startProg(someData, trainData, testData):
-#	X_train, X_test, y_train, y_test = train_test_split(trainData, testData, random_state=0)
-#	knn = Kn(n_neighbors=1)
-#	knn.fit(X_train, y_train)
-#	result = knn.predict(predictThis)
-#	if False:
-#		pass
-#	
-#someData = None
+def startProg(someData, input, target):
+	X_train, X_test, y_train, y_test = train_test_split(input, target, random_state=0)
+	knn = Kn(n_neighbors=1)
+	knn.fit(X_train, y_train)
+	result = knn.predict([someData])
+	print("predict() returned: ", result)
+	
+startProg([11, 19], inputValues, targetValues)
+	
 
 
