806960390491c63121604446d9708144c0e24b58
bob <dlee67@msudenver.edu>
2018-02-14 15:17:49 -0700

    02/14/18

--- Prac.py
+++ Prac.py
@@ -11,13 +11,26 @@ import mglearn
 import numpy as np
 
 cancer = load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=1)
+print(cancer['DESCR'])
+
+
+
+#X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=1)
+#svm = SVC(C=100)
+#scaler = MinMaxScaler()
+#
+#scaler.fit(X_train)
+#X_train_scaled = scaler.transform(X_train)
+#X_test_scaled = scaler.transform(X_test)
+#svm.fit(X_train_scaled, y_train)
+#print("Scaled test set accuracy: {:.2f}".format(
+#    svm.score(X_test_scaled, y_test)))
 
 #Demonstration that those two work pretty much the similar.
 #scaler = MinMaxScaler()	
 #PCA is super powerful...
 
-scaler = StandardScaler()
+#scaler = StandardScaler()
 #scaler.fit(cancer.data)
 #X_scaled = scaler.transform(cancer.data)
 #pca = PCA(n_components=2)
@@ -52,6 +65,6 @@ scaler = StandardScaler()
 #print("Scaled test set accuracy: {:.2f}".format(svm.score(X_test_scaled, y_test)))
 #svm.score(X_test_scaled, y_test)
 
-scaler.fit(X_train)
-X_train_scaled = scaler.transform(X_train)
-svm.score(X_test_scaled, y_test)
+#scaler.fit(X_train)
+#X_train_scaled = scaler.transform(X_train)
+#svm.score(X_test_scaled, y_test)
