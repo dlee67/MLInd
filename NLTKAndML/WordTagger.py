6363a30b1d04978b25866de176f44c7d296555b2
dlee67 <dlee67@msudenver.edu>
2018-03-28 19:02:23 -0600

    Turns out, read() doesn't return anything.

--- NLTKAndML/WordTagger.py
+++ NLTKAndML/WordTagger.py
@@ -10,16 +10,12 @@ class WordTagger(object):
 
     def innitFile(self, fileName):
         print("In the block of innitFile.")
-        self.needTagging = open(fileName, "r+")
+        self.needTagging = open(fileName, "r")
         self.afterReading = self.needTagging.read()
-        print("The contents of needTagging:\n", self.needTagging.read(),
-            "\nas a type of:", type(self.needTagging.read()))
 
     def tokenizeNeedTagging(self):
         print("In the block for tokenizeNeedTagging.")
         self.tokens = nltk.word_tokenize(self.afterReading)
-        print("tokens are now: ", self.tokens,
-                "\n as a type of: ", type(self.tokens))
 
     def toString(self):
         print("The needTagging is: ", self.needTagging.read(),
