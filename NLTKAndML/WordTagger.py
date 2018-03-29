0a3b8fa0c029232f684550fb18f6c361ebddb68d
dlee67 <dlee67@msudenver.edu>
2018-03-29 15:04:11 -0600

    The usual.

--- NLTKAndML/WordTagger.py
+++ NLTKAndML/WordTagger.py
@@ -6,19 +6,36 @@ import nltk
 class WordTagger(object):
 
     def __init__(self):
-        self.needTagging = None
+        self.isJobCounter = 0
+        self.needsTagging = None
         self.afterReading = None
         self.tokens = None
+        self.verboseMode = False
         print("WordTagger has been innitialized.")
 
-    def innitFile(self, fileName):
-        print("In the block of innitFile.")
-        self.needTagging = open(fileName, "r")
-        self.afterReading = self.needTagging.read()
+    def verboseModeOn(self):
+        self.verboseMode = True
 
-    def tokenizeNeedTagging(self):
-        print("In the block for tokenizeNeedTagging.")
+    def innitFile(self, fileName):
+        if(self.verboseMode):
+            print("In the block of innitFile.")
+        self.needsTagging = open(fileName, "r")
+        self.afterReading = self.needsTagging.read()
+
+    def tokenizeNeedsTagging(self):
+        if(self.verboseMode):
+            print("In the block for tokenizeNeedsTagging.")
         self.tokens = nltk.word_tokenize(self.afterReading)
+
+    def countFrequency(self, findThis):
+        fDistObj = nltk.FreqDist(self.tokens)
+        if(self.verboseMode):
+            print("Consumed:", fDistObj.keys())
+        amtOfOccur = fDistObj[findThis]
+        #I am guessing the frequency distribution cannot consume anything aside from
+        #tokenized string.
+        if(self.verboseMode):
+            print("The word", findThis, "has occured this much:", amtOfOccur)
 
     def toString(self):
         print("The needTagging is: ", self.needTagging.read(),
@@ -26,3 +43,10 @@ class WordTagger(object):
         print("The tokens are: ",  self.tokens,
             "\n as a type of: ", type(self.tokens))
         print("After reading: ", self.afterReading)
+
+    def listFunctions(self):
+        print("verboseModeOn <- Each functions prints out the results.\n",
+                "innitFile <- Consumes the text file, and prepares it to be tokenized.\n",
+                "tokenizeNeedsTagging <- Tokenizes the consumed text file.\n",
+                "countFrequency <- Counts the frequency of a certain string token.\n",
+                "toString <- prints out all the contents within the fields of WordTagger object, with it's data type.")
--- NLTKAndML/main.py
+++ NLTKAndML/main.py
@@ -1,7 +1,13 @@
 from WordTagger import WordTagger
+import sys
 
 if __name__ == "__main__":
   tagger = WordTagger()
+  print("Passed arguments: ", sys.argv[:])
+  if(sys.argv[1] == "-lm"):
+    tagger.listFunctions()
+  tagger.verboseModeOn()
   tagger.innitFile("sometext")
-  tagger.tokenizeNeedTagging()
-  tagger.toString()
+  tagger.tokenizeNeedsTagging()
+  #tagger.toString()
+  tagger.countFrequency("spam")
