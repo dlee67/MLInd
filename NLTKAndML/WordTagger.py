d4d511b0bb1594826b908f3fea95e5765805d137
dlee67 <dlee67@msudenver.edu>
2018-03-28 18:20:31 -0600

    After assigning the values to self.afterReading, by invoking read() from self.needTagging, things were able to be properly tokenized.

--- NLTKAndML/WordTagger.py
+++ NLTKAndML/WordTagger.py
@@ -1,23 +1,29 @@
-from nltk import nltk
+import nltk
 
-class WordTagger:
-
-    needTagging = None
-    tokens = None
+class WordTagger(object):
 
     def __init__(self):
+        self.needTagging = None
+        self.afterReading = None
+        self.tokens = None
         print("WordTagger has been innitialized.")
 
     def innitFile(self, fileName):
+        print("In the block of innitFile.")
         self.needTagging = open(fileName, "r+")
-        print("The contents of needTagging: ", self.needTagging.read())
+        self.afterReading = self.needTagging.read()
+        print("The contents of needTagging:\n", self.needTagging.read(),
+            "\nas a type of:", type(self.needTagging.read()))
 
     def tokenizeNeedTagging(self):
-        print("In the block for tokenizeNeedTagging");
-        temp = self.needTagging
-        tokens = nltk.word_tokenize(temp.read())
-        print("tokens are now: ",  tokens)
+        print("In the block for tokenizeNeedTagging.")
+        self.tokens = nltk.word_tokenize(self.afterReading)
+        print("tokens are now: ", self.tokens,
+                "\n as a type of: ", type(self.tokens))
 
     def toString(self):
-        print("The needTagging is: ", self.needTagging.read())
-        print("The tokens are: ",  self.tokens)
+        print("The needTagging is: ", self.needTagging.read(),
+                "\n as a type of: ", type(self.needTagging.read()))
+        print("The tokens are: ",  self.tokens,
+            "\n as a type of: ", type(self.tokens))
+        print("After reading: ", self.afterReading)
