cb84139d895fd67c9d85dadbf68cf2527317f263
dlee67 <dlee67@msudenver.edu>
2018-03-29 23:12:22 -0600

    Documentation is added.

--- NLTKAndML/WordTagger.py
+++ NLTKAndML/WordTagger.py
@@ -3,7 +3,13 @@
 import nltk
 
 class WordTagger(object):
-
+# The isCounter will be used to assign the boolean value of is category, after certain limit has passed.
+# needsTagging is the fresh text file, that needs to be tokenized.
+# afterReading is the text file in the form of string data type.
+# tokens are the string tokens fetched from the needsTagging.
+# triggerPattern is user specified string tokens, which will increment the isCounter.
+# verboseMode, when true, will obligate the WordTagger.py to verbosely explain everything that is going on.
+# isCategory will have to be assigned with a True value, if the isCounter exceeds the specified limit.
     def __init__(self):
         self.isCounter = 0
         self.needsTagging = None
@@ -14,23 +20,29 @@ class WordTagger(object):
         self.isCategory = False
         print("WordTagger has been innitialized.")
 
+# As the name suggests
     def printAfterReading(self):
         print("Consumed content\n:", self.afterReading, "\n")
 
+# As the name suggests
     def printTriggerPattern(self):
         print("The patterns for incrementing the counter is: ", self.triggerPattern[:])
 
+# As the name suggests
     def printCounter(self):
         print("Currently, the amount of counter is: ", self.isCounter)
 
+#   By comparing the tokens to the user specified patterns, increment the isCounter.
     def matchPattern(self):
         freqDist = nltk.FreqDist(self.tokens)
         for pattern in self.triggerPattern:
-            print("Current pattern: ", pattern)
-            print("Current frequency of the pattern: ", freqDist[pattern])
+            if(verboseMode == True):
+                print("Current pattern: ", pattern)
+                print("Current frequency of the pattern: ", freqDist[pattern])
             self.isCounter = self.isCounter + freqDist[pattern]
 
 #The method below was created with regex pattern in mind.
+# As the name suggests
     def addTriggerPattern(self, userInput):
         if(self.verboseMode):
             print("In the addTriggerPattern block.")
@@ -38,20 +50,26 @@ class WordTagger(object):
         if(self.verboseMode):
             print("The list of trigger pattern is: ", triggerPattern[:])
 
+# As the name suggests
     def verboseModeOn(self):
         self.verboseMode = True
 
+# As the name suggests
     def innitFile(self, fileName):
         if(self.verboseMode):
             print("In the block of innitFile.")
         self.needsTagging = open(fileName, "r")
         self.afterReading = self.needsTagging.read()
 
+# As the name suggests
     def tokenizeNeedsTagging(self):
         if(self.verboseMode):
             print("In the block for tokenizeNeedsTagging.")
         self.tokens = nltk.word_tokenize(self.afterReading)
 
+# Increment the isCounter separate from the matchPattern method, and string token specified by the user.
+# In this case, findThis argument will be used for comparison, and the isCounter will be incremented
+# according to that.
     def countFrequency(self, findThis, limit=3):
         fDistObj = nltk.FreqDist(self.tokens)
         if(self.verboseMode):
@@ -65,6 +83,7 @@ class WordTagger(object):
                 return;
             isCounter = isCounter + 1
 
+# As the name suggests
     def toString(self):
         print("The needTagging is: ", self.needTagging.read(),
                 "\n as a type of: ", type(self.needTagging.read()))
@@ -72,6 +91,7 @@ class WordTagger(object):
             "\n as a type of: ", type(self.tokens))
         print("After reading: ", self.afterReading)
 
+# As the name suggests
     def listFunctions(self):
         print("verboseModeOn <- Each functions prints out the results.\n",
                 "innitFile <- Consumes the text file, and prepares it to be tokenized.\n",
